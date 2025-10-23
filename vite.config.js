9264537a5bf826437783028ca8517910ad42c9bd
Nova <shibazakijunko6@gmail.com>
2025-10-23 17:38:05 +1300

    Fix deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '', 
+  base: '/scp-crud-website2/', 
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false
+  },
+  server: {
+    port: 3000
+  }
 })
